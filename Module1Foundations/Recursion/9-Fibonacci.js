ae2429b00814a7b6d49af8edadcc3cd231202530
ashikmoosa <ashikmoosa96@gmail.com>
2025-06-17 16:57:25 +0100

    Added tail recursion

--- Module1Foundations/Recursion/9-Fibonacci.js
+++ Module1Foundations/Recursion/9-Fibonacci.js
@@ -9,6 +9,7 @@ Input: n = 3
 Output: 2
 Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2. */
 
+// Time complexity - O(2^N)
 var fib = function (n) {
   // if (n == 0) return 0;
   // if (n == 1) return 1;
@@ -17,4 +18,11 @@ var fib = function (n) {
   else {
     return fib(n - 1) + fib(n - 2);
   }
+};
+
+// Optimised Tail Recursion - O(N)
+var fib = function (n, first, second) {
+  if (n == 0) return first;
+  if (n == 1) return second;
+  return fib(n - 1, second, first + second);
 };
