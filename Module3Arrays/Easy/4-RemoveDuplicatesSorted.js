a7d2946859ca16d7f4598ba01974285e9d50e470
ashikmoosa <ashikmoosa96@gmail.com>
2025-07-25 20:22:56 +0100

    Added easy array problems

--- Module3Arrays/Easy/4-RemoveDuplicatesSorted.js
+++ Module3Arrays/Easy/4-RemoveDuplicatesSorted.js
@@ -14,9 +14,10 @@ Explanation: Resulting array = [-2, 2, 4, 5, _, _, _, _]
 There are 4 distinct elements in nums and the elements marked as _ can have any value. */
 
 // Appr 1 - BF - one loop but splice check i+1 == i remove i check for neg numbers - O(n^2)
-// Appr 2 - Opt - 2 pointer tehnique - O(n)
+// Appr 2 - BS - using set since set only allows unique - O(n) but S(n)
+// Appr 3 - Opt - using 2 pointer technique - O(n) S(1)
 
-// Appr 1 - BF - O(n^2)
+// Appr 1 - BF - O(n^2) - tricky, dont incr in loop since array size changes when splicing
 function removeDuplicates(arr, n) {
   for (let i = 0; i < n; ) {
     if (arr[i] === arr[i + 1]) arr.splice(i, 1);
@@ -27,3 +28,32 @@ function removeDuplicates(arr, n) {
 }
 
 console.log(removeDuplicates([0, 0, 3, 3, 5, 6], 6));
+
+// Appr2 - Using another DS Set, set wont allow duplicates
+function removeDuplicatesUsingSet(arr, n) {
+  const set = new Set();
+  for (let i = 0; i < n; i++) {
+    set.add(arr[i]);
+  }
+
+  return set.size;
+}
+
+console.log(removeDuplicatesUsingSet([0, 0, 3, 3, 5, 6], 6));
+
+// Appr 3 - Using 2 pointer
+function removeDuplicatesUsingTwoPtr(arr, n) {
+  let i = 0;
+  j = 0;
+  while (j < n) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+    j++;
+  }
+  arr.length = i + 1;
+  return arr.length;
+}
+
+console.log(removeDuplicatesUsingTwoPtr([-2, 2, 4, 4, 4, 4, 5, 5], 8));
---
+++ Module3Arrays/Easy/5-RotateArray.js
@@ -0,0 +1,23 @@
+/* Given an integer array nums, rotate the array to the left by one.
+
+Examples:
+Input: nums = [1, 2, 3, 4, 5]
+Output: [2, 3, 4, 5, 1]
+Explanation: Initially, nums = [1, 2, 3, 4, 5]
+Rotating once to left -> nums = [2, 3, 4, 5, 1]
+
+Input: nums = [-1, 0, 3, 6]
+Output: [0, 3, 6, -1]
+Explanation: Initially, nums = [-1, 0, 3, 6]
+Rotating once to left -> nums = [0, 3, 6, -1] */
+
+function rotateArray(arr, n) {
+  let temp = arr[0];
+  for (let i = 1; i < n; i++) {
+    arr[i - 1] = arr[i];
+  }
+  arr[n - 1] = temp;
+  return arr;
+}
+
+console.log(rotateArray([-1, 0, 3, 6], 4));
